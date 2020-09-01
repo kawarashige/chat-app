d18101bc96a567ea82790397addccf59c3d54392
kawarashige <kawarashige23@gmail.com>
2020-09-01 19:12:48 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to: "messages#index"
+
   resources :messages
+
+  resources :users, only: [:edit, :update]
+
 end
